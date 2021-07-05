72e93beeeb0c55801d590215eb82e0b8592c65ac
SandraT <sndrbltr@gmail.com>
2021-07-05 18:06:27 +0000

    Delete and update solutions

--- js/components/Todo.js
+++ js/components/Todo.js
@@ -4,11 +4,26 @@ class Todo {
 
         this.DOM = null;
         this.listDOM = null;
+
+        this.addFormDOM = null;
         this.newMessageDOM = null;
         this.newBorderColorDOM = null;
         this.buttonSaveDOM = null;
 
-        this.messages = JSON.parse(localStorage.getItem('messages')) || [];
+        this.updateFormDOM = null;
+        this.updateMessageDOM = null;
+        this.updateBorderColorDOM = null;
+        this.buttonUpdateDOM = null;
+        this.buttonCancelDOM = null;
+
+        //this.latestUsedID = 0;
+        this.localStorageIDcount = 'todosID';
+        this.localStorageTodosKey = 'todosList';
+        // is localStorage bandom visas parasytas zinutes
+        this.latestUsedID = JSON.parse(localStorage.getItem(this.localStorageIDcount)) || 0;
+        this.messages = JSON.parse(localStorage.getItem(this.localStorageTodosKey)) || [];
+
+        this.currentlyEditableTaskID = 0;
 
         this.init();
     }
@@ -28,10 +43,11 @@ class Todo {
             console.error('ERROR: nerasta vieta, pagal duota selector');
             return false;
         }
-        //issipiesiam pradini turini
-        //console.log('piesiam...');
+        //sukutiam sarasui klase
         this.DOM.classList.add('todo');
 
+        //issipiesiam pradini turini
+        //console.log('piesiam...');
         this.render();
         this.renderList();
         this.addEvents();
@@ -47,24 +63,24 @@ class Todo {
     }
 
     generateAddForm() {
-        return `<form>
+        return `<form id="add_task">
                     <label for="new_text">Message</label>
-                    <input id="new_text" type="text">
+                    <input id="new_text" type="text" value="">
                     <label for="new_border_color">Border color</label>
-                    <input id="new_border_color" type="color">
+                    <input id="new_border_color" type="color" value="#ff1100">
                     <button id="save_button" type="submit">Save</button>
                     <button type="reset">Reset</button>
                 </form>`;
     }
 
     generateUpdateForm() {
-        return `<form class="hide">
+        return `<form id="update_task" class="hide">
                     <label for="updated_text">Message</label>
                     <input id="updated_text" type="text">
                     <label for="updated_border_color">Border color</label>
                     <input id="updated_border_color" type="color">
-                    <button type="submit">Update</button>
-                    <button type="button">Cancel</button>
+                    <button id="update_button" type="submit">Update</button>
+                    <button id="cancel_button" type="button">Cancel</button>
                 </form>`;
     }
 
@@ -74,16 +90,16 @@ class Todo {
 
     renderList() {
         for (const task of this.messages) {
-            this.renderTask(task.messageText, task.borderColor);
+            this.renderTask(task.id, task.messageText, task.borderColor);
         }
     }
 
-    renderTask(text, borderColor = '#ccc') {
+    renderTask(id, text, borderColor = '#ccc') {
         if (typeof text !== 'string' ||
             text === '') {
             return '';
         }
-        const HTML = `<div class="task" style="border-color: ${borderColor}">
+        const HTML = `<div id="task_${id}" class="task" style="border-color: ${borderColor}">
                             <div class="text">${text}</div>
                             <div class="actions">
                                 <div class="btn edit">Edit</div>
@@ -92,6 +108,30 @@ class Todo {
                         </div>`;
 
         this.listDOM.insertAdjacentHTML('afterbegin', HTML);
+
+        const taskDOM = this.listDOM.querySelector('.task');
+        const editDOM = taskDOM.querySelector('.edit');
+        const deleteDOM = taskDOM.querySelector('.delete');
+
+        deleteDOM.addEventListener('click', () => {
+            if (!confirm('Ar tikrai norite istrinti si irasa?')) {
+                return false;
+            }
+
+            taskDOM.remove();
+
+            this.messages = this.messages.filter((task) => task.id !== id);
+            localStorage.setItem(this.localStorageTodosKey, JSON.stringify(this.messages));
+        })
+
+        editDOM.addEventListener('click', () => {
+            this.addFormDOM.classList.add('hide');
+            this.updateFormDOM.classList.remove('hide');
+
+            this.updateMessageDOM.value = text;
+            this.updateBorderColorDOM.value = borderColor;
+            this.currentlyEditableTaskID = id;
+        })
     }
 
     render() {
@@ -102,14 +142,24 @@ class Todo {
         this.DOM.innerHTML = HTML;
 
         this.listDOM = this.DOM.querySelector('.list');
+
+        this.addFormDOM = document.getElementById('add_task');
         this.newMessageDOM = document.getElementById('new_text');
         this.newBorderColorDOM = document.getElementById('new_border_color');
         this.buttonSaveDOM = document.getElementById('save_button');
+
+        this.updateFormDOM = document.getElementById('update_task');
+        this.updateMessageDOM = document.getElementById('updated_text');
+        this.updateBorderColorDOM = document.getElementById('updated_border_color');
+        this.buttonUpdateDOM = document.getElementById('update_button');
+        this.buttonCancelDOM = document.getElementById('cancel_button');
     }
 
     addEvents() {
+        // Pridedamas irasas
         this.buttonSaveDOM.addEventListener('click', (e) => {
             e.preventDefault();
+
             const message = this.newMessageDOM.value;
             const color = this.newBorderColorDOM.value;
 
@@ -117,15 +167,48 @@ class Todo {
                 return false;
             }
 
-            this.renderTask(message, color);
-
             this.messages.push({
+                id: ++this.latestUsedID,
                 messageText: message,
                 borderColor: color
             })
 
-            localStorage.setItem('messages', JSON.stringify(this.messages));
+            this.renderTask(this.latestUsedID, message, color);
+
+            // atnaujina info, kuri yra atminty
+            //localStorage.setItem(this.localStorageTodoKey, JSON.stringify(this.messages));
+            localStorage.setItem(this.localStorageIDcount, JSON.stringify(this.latestUsedID));
+            localStorage.setItem(this.localStorageTodosKey, JSON.stringify(this.messages));
             //console.log(this.messages);
+        })
+        // iraso istrynimas
+        this.buttonCancelDOM.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.addFormDOM.classList.remove('hide');
+            this.updateFormDOM.classList.add('hide');
+        })
+
+        //iraso redagavimas
+        this.buttonUpdateDOM.addEventListener('click', (e) => {
+            e.preventDefault();
+            const message = this.updateMessageDOM.value;
+            const color = this.updateBorderColorDOM.value;
+
+            for (const task of this.messages) {
+                if (task.id === this.currentlyEditableTaskID) {
+                    task.messageText = message;
+                    task.borderColor = color;
+                }
+            }
+            localStorage.setItem(this.localStorageTodosKey, JSON.stringify(this.messages));
+
+            const taskDOM = this.DOM.querySelector('#task_' + this.currentlyEditableTaskID);
+            const taskTextDOM = taskDOM.querySelector('.text');
+            taskTextDOM.innerText = message;
+            taskDOM.style.borderColor = color;
+
+            this.addFormDOM.classList.remove('hide');
+            this.updateFormDOM.classList.add('hide');
         })
     }
 }
